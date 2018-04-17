1225c5139d10f06c7285900da6cb6bc1461dbadd
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-04-17 13:42:45 -0500

    Some updated docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0+untagged.475.g33255d3.dirty',
+    VERSION: '0+untagged.509.g048e498.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
