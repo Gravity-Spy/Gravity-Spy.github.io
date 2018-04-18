da7e59bbd1e473bda536818e15ebbf555480312b
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-04-18 13:59:09 -0500

    Alright now these docs are headed in the right direction

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0+untagged.509.g048e498.dirty',
+    VERSION: '0+untagged.511.g4e019ea.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
