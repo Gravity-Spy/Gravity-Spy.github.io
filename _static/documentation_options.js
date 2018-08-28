43e82c2a5d01a514c0f8c0ce9b1c4c08e4816fd9
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-08-28 15:31:30 +0100

    New docs to go with new API interface tot he code

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '0+untagged.528.ge27167b.dirty',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.1.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
