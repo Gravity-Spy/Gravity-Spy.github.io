72e6e17d88830b393d0a06be2be038d9ed18a852
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-09-27 10:10:15 -0500

    The module level ldocs broke?

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0+13.gbdf4d08.dirty',
+    VERSION: '0.1.0+24.gac12833.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
