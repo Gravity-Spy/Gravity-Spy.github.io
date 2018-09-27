1a4e70e338fb7f89587254ba6c6b01de84908e59
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-09-27 10:41:26 -0500

    Mistake in new docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0+24.gac12833.dirty',
+    VERSION: '0.1.0+25.g4998762.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
